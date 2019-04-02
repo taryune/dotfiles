2fbc5006b3390dbff27f9af76591fcbe9eba0aa6
taryune <taryune@omochi.local>
2019-04-02 19:02:04 +0900

    add sh

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+
