89d7a16373f55aca8318ce1fdd55f7d733075f7a
tarumi <taryunelab@gmail.com>
2024-06-06 19:16:07 +0200

    update to codegemma

--- nvim/lua/config/chatgpt.lua
+++ nvim/lua/config/chatgpt.lua
@@ -2,6 +2,9 @@
 local config = {
   api_host_cmd = "echo -n http://localhost:11434",
   api_key_cmd = "echo ollama",
+  openai_params = {
+    model = "codegemma",
+  },
 }
 
 require("chatgpt").setup(config)
